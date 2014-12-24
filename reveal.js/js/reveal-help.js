19944ab241e965f06fd17f33a77dcd5e6a75d72d
Vladimir Alexiev <vladimir.alexiev@ontotext.com>
2014-12-24 10:03:59 +0200

    reveal-help.js

--- reveal.js/js/reveal-help.js
+++ reveal.js/js/reveal-help.js
@@ -1,11 +1,11 @@
 Reveal.configure ({mouseWheel:true, previewLinks:false, viewDistance:4,
   keyboard:{
     85:function(){ // U=show slide number in URL/history
-      // var r = Reveal.getIndices();
-      Reveal.configure({history:!window.location.hash}); 
-      if (!!window.location.hash) {window.location.hash=''}; 
-      window.location.reload;
-      // Reveal.slide(r.h,r.v)
+      var r = Reveal.getIndices();
+      Reveal.configure({history : !Reveal.getConfig().history}); 
+      Reveal.slide(r.h,r.v);
+      if (!Reveal.getConfig().history) {window.location.hash=''}; 
+      //window.location.reload;
       // unfortunately goes to first slide when history is turned off, last line doesn't fix
       // https://github.com/hakimel/reveal.js/issues/934
     },
