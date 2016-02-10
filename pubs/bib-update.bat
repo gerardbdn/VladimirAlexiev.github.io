c2e2b4decb4b8727de7f46ebcf05912bc16d6e7c
Vladimir Alexiev <vladimir.alexiev@ontotext.com>
2016-02-10 12:31:05 +0200

    switch to BibBase

--- pubs/bib-update.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM bib2xhtml calls bibtex that uses files only in the cwd, so we should work there
-REM It also writes tmp files in current dir, so under Win7, the dir cannot be in C:\Program Files (x86)
-cd bib2xhtml
-REM -n Alexiev to bold author name
-perl bib2xhtml -h "Vladimir Alexiev Bibliography" -c -r -t ../Alexiev-bibliography.bib ../index.html
-cd ..
-
-
